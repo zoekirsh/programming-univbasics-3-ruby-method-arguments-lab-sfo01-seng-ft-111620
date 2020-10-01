b3d8baca018e7cc6bb2014699e11866f7ba88372
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-01 19:25:48 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,11 +6,10 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-introduction_with_language ("Dan", "Ember.js")
 
 
 def introduction_with_language_optional (name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning #{language}."
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
 
